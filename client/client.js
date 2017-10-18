d3474344f16f0d0f27b34d2281a5d1ed1aa9f5e4
Alexey S. Kiselev <alexeys.kiselev@yahoo.com>
2017-10-19 01:17:20 +0300

    0.0.2 - Add Promise-like request function to client.js

--- client/client.js
+++ client/client.js
@@ -1,2 +1,57 @@
-// Edit me.
-// Feel free to add other JS files in this directory as you see fit.
+/**
+ * Promise-like request function
+ * @param options - object with request options:
+ *      method: string - REST methods (CRUD) - Required
+ *      url: string - request URL - Required
+ *      params: string or object - request params - Optional
+ *      headers: object - request Headers - Optional
+ */
+
+function request(options){
+    return new Promise((resolve, reject) => {
+        // Params
+        // If params is a String
+        let params = options.params || '';
+        // If params is an Object
+        if(options.params && typeof options.params === 'object'){
+            params = Object.keys(options.params).map(param => {
+                return encodeURIComponent(param) + '=' + encodeURIComponent(params[param]);
+            }).join('&');
+        }
+
+        let xhr = new XMLHttpRequest();
+        if(options.method === 'GET'){
+            xhr.open(options.method, options.url+'?'+params, true);
+        } else {
+            xhr.open(options.method, options.url, true);
+        }
+
+        xhr.onload = () => {
+            let response = JSON.parse(xhr.response);
+            if(response.error){
+                reject(response);
+            } else {
+                resolve(response);
+            }
+        };
+        xhr.onerror = () => {
+            reject({
+                status: xhr.status,
+                statusText: xhr.statusText
+            });
+        };
+
+        // Default headers
+        let headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+        // Assign headers
+        headers = Object.assign(options.headers, headers);
+        // Headers
+        Object.keys(headers).map(header => {
+            xhr.setRequestHeader(header, headers[header]);
+        });
+
+        xhr.send(params);
+    });
+}
